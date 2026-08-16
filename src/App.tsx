7ee41338a3f97a3c579396a6c3564ee10ec1eac6
Saikhan <saykhanabu123@gmail.com>
2026-08-16 11:53:42 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-500">Workout Tracker</h1>
+      <p className="mt-4 text-gray-600">Meine erste React App</p>
+    </div>
+  )
+}
+
+export default App
